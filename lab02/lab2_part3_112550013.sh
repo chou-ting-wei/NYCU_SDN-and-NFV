f3241b3cce6c989f5ee895c55f77861fb1a92a8e
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-09-19 20:28:01 +0800

    add lab02

--- lab02/lab2_part3_112550013.sh
+++ lab02/lab2_part3_112550013.sh
@@ -2,3 +2,12 @@ sudo mn --custom=topo_112550013.py \
 --topo=topo_112550013 \
 --controller=remote,ip=127.0.0.1:6653 \
 --switch=ovs,protocols=OpenFlow14
+
+curl -u onos:rocks -X POST -H 'Content-Type: application/json' -d @flows_s1-1_112550013.json \
+'http://localhost:8181/onos/v1/flows/of:0000000000000001'
+
+curl -u onos:rocks -X POST -H 'Content-Type: application/json' -d @flows_s2-1_112550013.json \
+'http://localhost:8181/onos/v1/flows/of:0000000000000002'
+
+curl -u onos:rocks -X DELETE -H 'Accept: application/json' \
+'http://localhost:8181/onos/v1/flows/of:0000000000000001/${flow_id}'
